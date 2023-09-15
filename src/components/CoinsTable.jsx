b701e1d5222031a478e35c0bdfbc594b9a1ae8f2
hardikgupta2003 <hardikgupta5172@gmail.com>
2023-09-15 23:55:55 +0530

    Created Coins Table

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -1,12 +1,135 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CryptoState } from '../CryptoContext';
+import {
+  Container,
+  LinearProgress,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+  makeStyles
+} from '@material-ui/core';
+import { CoinList } from '../config/api';
+import axios from 'axios';
 
 
 export function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 const CoinsTable = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+  const { currency, symbol } = CryptoState();
+
+  const FetchCoinsData = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+    console.log(data);
+    setCoins(data);
+    setLoading(false);
+  }
+  useEffect(() => {
+    FetchCoinsData();
+
+  }, [currency]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const useStyles= makeStyles({
+    row:{
+      backgroundColor: "#16171a",
+      cursor: "pointer",
+      "&:hover": {
+        backgroundColor: "#131111",
+      },
+      fontFamily: "Montserrat",
+    },
+    pagination: {
+      "& .MuiPaginationItem-root": {
+        color: "gold",
+      },
+    }
+  })
+
   return (
-    <div>CoinsTable</div>
+    <ThemeProvider theme={darkTheme}>
+
+      <Container style={{ textAlign: "center" }}>
+        <Typography>
+          cryptocurrency Prices By market Cap
+        </Typography>
+
+        <TextField
+          label="Search For a Crypto Currency.."
+          variant="outlined"
+          style={{
+            marginBottom: 20,
+            width: "100%"
+          }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <TableContainer>
+          {loading ? (<LinearProgress style={{ backgroundColor: "gold" }} />)
+            :
+            (<Table aria-label="simple table">
+              <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                <TableRow>
+                  {["coin","Price","24h change","Market Cap"].map((head)=>{
+                    <TableCell
+                    style={{
+                      color:"black",
+                      fontWeight:"700",
+                      fontFamily:"Montserrat",
+                    }}
+                    key={head}
+                    align={head === "coin" ? "" : "right"}
+                    >
+                      {head}
+                  </TableCell>
+                  })}
+                  
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {handleSearch().map((row)=>{
+                  const profit =row.price_change_percentage_24h > 0;
+                  return (
+                    <TableRow onClick={
+                      ()=> history.pushState(`/coins/${row.id}`)}
+                      className={Classnames.row}
+                      key={row.name}>
+
+                    </TableRow>
+                  )
+                })}
+              </TableBody>
+            </Table>)
+          }
+        </TableContainer>
+
+
+
+      </Container>
+    </ThemeProvider>
   )
 }
 
